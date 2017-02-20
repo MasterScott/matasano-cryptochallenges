ebaa3248c097da36584c428af1cc34a830321a97
Dimitris Karakostas <dimit.karakostas@gmail.com>
2017-02-20 22:03:08 +0200

    fixup! Add problem 7

--- problem_7.py
+++ problem_7.py
@@ -1,13 +1,24 @@
 from Crypto.Cipher import AES
 
-KEY = 'YELLOW SUBMARINE'
-IV = KEY.encode('hex').upper()
 
-with open('7.txt', 'r') as f:
-    base64_encoded_input = ''.join(f.readlines())
+def ecb_aes(text, key, blocksize):
+    aes_obj = AES.new(key)
+    blocks = [text[i*blocksize:(i+1)*blocksize] for i in range(len(text)/blocksize)]
+    cleared_blocks = []
+    for block in blocks:
+        cleared_block = aes_obj.decrypt(block)
+        cleared_blocks.append(cleared_block)
+    return ''.join(cleared_blocks)
 
-ciphertext = base64_encoded_input.decode('base64')
-aes_obj = AES.new(KEY, AES.MODE_ECB, IV=IV)
-plaintext = aes_obj.decrypt(ciphertext)
 
-print plaintext
+if __name__ == '__main__':
+    KEY = 'YELLOW SUBMARINE'
+    BLOCKSIZE = 16
+
+    with open('7.txt', 'r') as f:
+        base64_encoded_input = ''.join(f.readlines())
+
+    ciphertext = base64_encoded_input.decode('base64')
+    plaintext = ecb_aes(ciphertext, KEY, BLOCKSIZE)
+
+    print plaintext
